5843cb826b58aa0107de41bb96c601586b12f10d
shinobinana <rachana.k.kulkarni@gmail.com>
2025-05-12 11:36:41 +0530

    Update cache22.h

--- cache22/cache22.h
+++ cache22/cache22.h
@@ -1,43 +1,189 @@
-/*cache22.h*/
+/*cache22.c*/
 
-#ifndef CACHE22
-#define CACHE22
+#include "cache22.h"
 
-#define _GNU_SOURCE
+bool scontinuation;
+bool ccontinuation;
 
-#include<stdio.h>
-#include<unistd.h>
-#include<string.h>
-#include<stdlib.h>
-#include<stdbool.h>
-#include<stddef.h>
-#include<stdarg.h>
+void zero(int8* buf,int16 size){
+        int8 *p;
+        int16 n;
 
-#include<assert.h>
-#include<errno.h>
+        for(n=0,p=buf; n<size; n++,p++)
+                *p = 0;
+}
 
-#include<arpa/inet.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
+void childloop(Client *cli){
+        int8 buf[256];
+        int16 n;
+        int8 *p, *f;
+        int8 cmd[256], folder[256], args[256];
 
-#define HOST    "127.0.0.1"
-#define PORT    "12049"
+        zero(buf,256);
+        read(cli->s, (char *)buf, 255);
 
-typedef unsigned int int32;
-typedef unsigned short int int16;
-typedef unsigned char int8;
+        n = (int16)strlen((char*)buf);
+        if(n>254)
+                n = 254;
 
-struct s_client {
-        int s;
-        char ip[16];
+        for(p=buf;
+                        (*p)
+                        && (n--)
+                        && (*p != ' ')
+                        && (*p != '\n')
+                        && (*p != '\r');
+        p++
+                );
+        zero(cmd, 256); zero(folder, 256); zero(args, 256);
+        if(!(*p)||(!n)) {
+                strncpy((char *)cmd,(char *)buf, 255);
+                goto end;
+        }
+
+        else if(
+                (*p == '\n')
+                || (*p =='\r')){
+        *p =0;
+        strncpy((char *)cmd, (char *)buf, 255);
+        goto end;
+        }
+
+        else if (*p == ' '){
+                *p = 0;
+                strncpy((char *)cmd, (char *)buf, 255);
+
+        }
+
+        for(p++, f=p;
+                        (*p)
+                        && (n--)
+                        && (*p != ' ')
+                        && (*p != '\n')
+                        && (*p != '\r');
+        p++
+        );
+        if(!(*p)||(!n)) {
+                strncpy((char *)folder,(char *)f, 255);
+                goto end;
+        }
+        else if((*p == ' ')
+                || (*p == '\n')
+                || (*p =='\r'))
+        {
+                *p = 0;
+                strncpy((char *)folder, (char *)f, 255);
+        }
+
+        p++;
+        if(*p) {
+                strncpy((char*)args, (char *)p, 255);
+                for(p=args;
+                                (*p)
+                                && (*p != '\n')
+                                && (*p != '\r'); p++);
+                        *p = 0;
+        }
+
+
+end:
+        dprintf(cli->s, "\ncmd:\t%s\n",cmd);
+        dprintf(cli->s, "\nfolder:\t%s\n",folder);
+        dprintf(cli->s, "\nargs:\t%s\n",args);
+        return;
+}
+
+void mainloop(int s) {
+        struct sockaddr_in cli;
+        int s2;
+        int32 len;
+        char *ip;
         int16 port;
-};
-typedef struct s_client Client;
+        Client *client;
+        pid_t pid;
 
-void childloop(Client*);
-void zero(int8*,int16);
-void mainloop(int);
-int initserver(int16);
-int main(int,char**);
+        s2 = accept(s,(struct sockaddr *)&cli, (unsigned int *)&len);
+        if(s2<0)
+                return;
 
-#endif
+        port = (int16)htons((int)cli.sin_port);
+        ip = inet_ntoa(cli.sin_addr);
+
+        printf("Connection from %s:%d\n",ip,port);
+
+        client = (Client *)malloc(sizeof(struct s_client));
+        assert(client);
+
+        zero((int8 *)client, sizeof(struct s_client));
+        client->s = s2;
+        client->port = port;
+        strncpy(client->ip, ip, 15);
+
+        pid = fork();
+        if(pid) {
+                free(client);
+
+                return;
+        }
+        else {
+                dprintf(s2, "100 connected to cache22 server\n");
+                ccontinuation = true;
+                while(ccontinuation)
+                        childloop(client);
+
+                close(s2);
+                free(client);
+
+                return;
+        }
+
+        return;
+}
+
+int initserver(int16 port) {
+        struct sockaddr_in sock;
+        int s;
+
+        sock.sin_family = AF_INET;
+        sock.sin_port = htons((int)port);
+        sock.sin_addr.s_addr = inet_addr(HOST);;
+
+        s = socket(AF_INET, SOCK_STREAM, 0);
+        assert(s>0);
+
+        errno = 0;
+        if (bind(s, (struct sockaddr *)&sock, sizeof(sock)))
+                assert_perror(errno);
+
+        errno = 0;
+        if (listen(s,20))
+                assert_perror(errno);
+
+        printf("Server listening on %s:%d\n", HOST, port);
+
+        return s;
+}
+
+int main(int argc, char *argv[]){
+
+        char *sport;
+        int16 port;
+        int s;
+
+        if(argc <2)
+                sport = PORT;
+        else
+                sport = argv[1];
+
+        port = (int16)atoi(sport);
+
+        s = initserver(port);
+        scontinuation = true;
+        while(scontinuation)
+                mainloop(s);
+
+        printf("Shutting down...\n");
+        close(s);
+        return 0;
+}
+
+#pragma GCC diagnostic pop
