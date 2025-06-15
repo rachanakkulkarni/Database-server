8af7cded2534ecdc208be29fa31f5e265e0ef557
Rachana Kulkarni <rachana.k.kulkarni@gmail.com>
2025-06-16 00:38:10 +0530

    Update cache22.c

--- cache22/cache22.c
+++ cache22/cache22.c
@@ -1,52 +1,256 @@
-/*cache22.h*/
+/*cache22.c*/
+#include "cache22.h"
 
-#ifndef CACHE22
-#define CACHE22
+extern Node root;
 
-#define _GNU_SOURCE
+int32 handle_hello(Client*, int8*, int8*);
 
-#include<stdio.h>
-#include<unistd.h>
-#include<string.h>
-#include<stdlib.h>
-#include<stdbool.h>
-/*
-#include<stddef.h>
-#include<stdarg.h>
-*/
-#include<assert.h>
-#include<errno.h>
+bool scontinuation;
+bool ccontinuation;
 
-#include<arpa/inet.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include "tree.h"
+CmdHandler handlers[] = {
+        {(int8*)"hello",handle_hello}
+};
 
-#define HOST    "127.0.0.1"
-#define PORT    "12049"
+Callback getcmd(int8 *cmd){
+        Callback cb;
+        int16 n, arrlen;
 
-typedef unsigned int int32;
-typedef unsigned short int int16;
-typedef unsigned char int8;
+        if(sizeof(handlers) <16)
+                return 0;
+        arrlen = (sizeof(handlers)/16);
 
-struct s_client {
-        int s;
-        char ip[16];
+        cb = 0;
+
+        for(n=0; n<arrlen; n++)
+                if(!strcmp((char *)cmd,
+                                        (char *)handlers[n].cmd)){
+                        cb = handlers[n].handler;
+                        break;
+                }
+        return cb;
+
+}
+
+int32 handle_hello(Client *cli, int8 *folder, int8 *args)
+{
+        dprintf(cli->s, "hello '%s'\n", folder);
+
+        return 0;
+}
+
+void childloop(Client *cli){
+        int8 buf[256];
+        int16 n;
+        int8 *p, *f;
+        int8 cmd[256], folder[256], args[256];
+        Callback cb;
+
+        zero(buf,256);
+        read(cli->s, (char *)buf, 255);
+
+        n = (int16)strlen((char*)buf);
+        if(n>254)
+                n = 254;
+
+        for(p=buf;
+                        (*p)
+                        && (n--)
+                        && (*p != ' ')
+                        && (*p != '\n')
+                        && (*p != '\r');
+        p++
+                );
+        zero(cmd, 256); zero(folder, 256); zero(args, 256);
+        if(!(*p)||(!n)) {
+                strncpy((char *)cmd,(char *)buf, 255);
+                goto end;
+        }
+
+        else if(
+                (*p == '\n')
+                || (*p =='\r')){
+        *p =0;
+        strncpy((char *)cmd, (char *)buf, 255);
+        goto end;
+        }
+
+        else if (*p == ' '){
+                *p = 0;
+                strncpy((char *)cmd, (char *)buf, 255);
+
+        }
+
+        for(p++, f=p;
+                        (*p)
+                        && (n--)
+                        && (*p != ' ')
+                        && (*p != '\n')
+                        && (*p != '\r');
+        p++
+        );
+        if(!(*p)||(!n)) {
+                strncpy((char *)folder,(char *)f, 255);
+                goto end;
+        }
+        else if((*p == ' ')
+                || (*p == '\n')
+                || (*p =='\r'))
+        {
+                *p = 0;
+                strncpy((char *)folder, (char *)f, 255);
+        }
+
+        p++;
+        if(*p) {
+                strncpy((char*)args, (char *)p, 255);
+                for(p=args;
+                                (*p)
+                                && (*p != '\n')
+                                && (*p != '\r'); p++);
+                        *p = 0;
+        }
+
+
+end:
+        dprintf(cli->s, "\ncmd:\t%s\n",cmd);
+        dprintf(cli->s, "\nfolder:\t%s\n",folder);
+        dprintf(cli->s, "\nargs:\t%s\n",args);
+
+        cb = getcmd(cmd);
+        if(!cb){
+                dprintf(cli->s,"400 Command not found %s\n", cmd);
+                return;
+        }
+        else {
+                cb(cli, folder, args);
+                return;
+        }
+
+        return;
+}
+
+void mainloop(int s) {
+        struct sockaddr_in cli;
+        int s2;
+        int32 len;
+        char *ip;
         int16 port;
-};
-typedef struct s_client Client;
+        Client *client;
+        pid_t pid;
 
-typedef int (*Callback)(Client*, int8*, int8*);
+        s2 = accept(s,(struct sockaddr *)&cli, (unsigned int *)&len);
+        if(s2<0)
+                return;
 
-struct s_cmdhandler {
-        int8* cmd;
-        Callback handler;
-};
-typedef struct s_cmdhandler CmdHandler;
+        port = (int16)htons((int)cli.sin_port);
+        ip = inet_ntoa(cli.sin_addr);
 
-void childloop(Client*);
-void mainloop(int);
-int initserver(int16);
-int main(int,char**);
+        printf("Connection from %s:%d\n",ip,port);
 
-#endif
+        client = (Client *)malloc(sizeof(struct s_client));
+        assert(client);
+
+        zero((int8 *)client, sizeof(struct s_client));
+        client->s = s2;
+        client->port = port;
+        strncpy(client->ip, ip, 15);
+
+        pid = fork();
+        if(pid) {
+                free(client);
+
+                return;
+        }
+        else {
+                dprintf(s2, "100 connected to cache22 server\n");
+                ccontinuation = true;
+                while(ccontinuation)
+                        childloop(client);
+
+                close(s2);
+                free(client);
+
+                return;
+        }
+
+        return;
+}
+
+int initserver(int16 port) {
+        struct sockaddr_in sock;
+        int s;
+
+        sock.sin_family = AF_INET;
+        sock.sin_port = htons((int)port);
+        sock.sin_addr.s_addr = inet_addr(HOST);;
+
+        s = socket(AF_INET, SOCK_STREAM, 0);
+        assert(s>0);
+
+        errno = 0;
+        if (bind(s, (struct sockaddr *)&sock, sizeof(sock)))
+                assert_perror(errno);
+
+        errno = 0;
+        if (listen(s,20))
+                assert_perror(errno);
+
+        printf("Server listening on %s:%d\n", HOST, port);
+
+        return s;
+}
+
+int main(int argc, char *argv[]){
+
+        char *sport;
+        int16 port;
+        int s;
+        Node *n, *n2;
+        Leaf *l;
+        int8 *p;
+        int16 sz;
+
+        p = (int8 *)"true";
+        sz = (int16)strlen((char *)p);
+
+        n = create_node(&root, (int8 *)"/Users/");
+        printf("n\t%p\n", n);
+
+        n2 = create_node(n, (int8 *)"/Users/jobj");
+        printf("n2\t%p\n", n2);
+
+        l = create_leaf(n, (int8 *)"loggedin",
+                        p,sz);
+        printf("l\t%p\n", l);
+
+        free(n2);
+        free(n);
+        exit(0);
+
+//      Callback x;
+
+/*      x = getcmd((int8 *)"hello");
+        printf("%p\n",x);
+
+        x = getcmd((int8 *)"skdkjdkjfn");
+        printf("%p\n",x);
+*/
+        if(argc <2)
+                sport = PORT;
+        else
+                sport = argv[1];
+
+        port = (int16)atoi(sport);
+
+        s = initserver(port);
+        scontinuation = true;
+        while(scontinuation)
+                mainloop(s);
+
+        printf("Shutting down...\n");
+        close(s);
+        return 0;
+}
+
+#pragma GCC diagnostic pop
